c5bda121f7733bacfc516223c8d08af0f1879d9e
Robbie Jaeger <jaeger.rob@gmail.com>
2018-09-09 15:20:36 -0600

    Add knexfile config

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/plants',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/plants_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  }
+};
