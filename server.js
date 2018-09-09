752308bc183ccbc8e080ffea228b511f06f64b42
Robbie Jaeger <jaeger.rob@gmail.com>
2018-09-09 15:30:25 -0600

    Add knex env config to server file

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || 3000);
 
